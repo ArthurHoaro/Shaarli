d21e40c8821c7e761343181945c576660a070b6e
ArthurHoaro <arthur@hoa.ro>
2018-01-31 15:30:38 +0100

    Manage frontend dependencies with npm/yarn and webpack
    Relates to #755
    
      - move default theme assets to /assets/default
      - asset folders in default template are now git ignored
      - handle assets dependencies with yarn (and fixed version with yarn.lock)
      - handle assets compilation with webpack 2
    
    JS:
      - use ES6 syntax
      - compile it with babel
      - minify it using babel-minify-webpack-plugin
      - initiate splitting JS files into separate modules (base/picwall/[todo])
      - dependencies are now bundled with Shaarli's JS file (awesomplete and blazy)
    
    CSS:
      - move Shaarli's pure CSS to SASS
      - dependencies are now bundled with Shaarli's CSS file (awesomplete/purecss/fontawesome)
      - CSS is now minified
    
    Images:
      - compressed at compilation by image-webpack-loader

---
+++ assets/default/js/picwall.js
@@ -0,0 +1,11 @@
+import Blazy from 'blazy';
+
+window.onload = function () {
+    /**
+     * bLazy trigger
+     */
+    let picwall = document.getElementById('picwall_container');
+    if (picwall != null) {
+        let bLazy = new Blazy();
+    }
+};
