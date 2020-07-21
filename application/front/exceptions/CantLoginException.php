7fa85bd8c584cb8222b7fc4176ff3849abfb3215
ArthurHoaro <arthur@hoa.ro>
2020-07-21 20:33:33 +0200

    Process login through Slim controller

---
+++ application/front/exceptions/CantLoginException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author      Wizacha DevTeam <dev@wizacha.com>
+ * @copyright   Copyright (c) Wizacha
+ * @license     Proprietary
+ */
+
+declare(strict_types=1);
+
+namespace Shaarli\Front\Exception;
+
+class CantLoginException extends \Exception
+{
+
+}
